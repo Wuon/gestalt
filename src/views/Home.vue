7cddd743af310061b2d6f106f936246e62bcfd9d
Daniel Wu <wuon@protonmail.com>
2019-06-23 22:02:26 -0700

    plain design

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,16 @@
 <template>
-  <div class="home">
-    <p>{{quote.date}}</p>
-    <h1>{{quote.msg}}</h1>
-    <p>{{quote.context}}</p>
-    <h5>- {{quote.owner}}</h5>
+  <div class="quote-container">
+    <div class="quote-top">
+      <p>{{cur}} / {{length}}</p>
+    </div>
+    <div class="quote-middle">
+      <h1>"{{quote.msg}}"</h1>
+      <p>{{quote.context}}</p>
+      <h5>- {{quote.owner}}</h5>
+    </div>
+    <div class="quote-bottom">
+      <p>{{quote.date}}</p>
+    </div>
   </div>
 </template>
 
@@ -19,6 +26,24 @@ import json from "@/quotes.json";
 })
 export default class Home extends Vue {
   quotes: any = json.quotes;
+  cur: number = 1;
+  length: number = this.quotes.length;
   quote: any = this.quotes[0];
 }
 </script>
+
+<style scoped>
+.quote-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 100vh;
+  width: 100%;
+  align-content: flex-start;
+}
+.quote-middle,
+.quote-top,
+.quote-bottom {
+  padding: 25px;
+}
+</style>
