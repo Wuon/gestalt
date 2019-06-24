dcfe0f4a4e34b9562adb5e631dc6dd534de353a3
Daniel Wu <wuon@protonmail.com>
2019-06-23 21:44:05 -0700

    quote template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>{{quote.date}}</p>
+    <h1>{{quote.msg}}</h1>
+    <p>{{quote.context}}</p>
+    <h5>- {{quote.owner}}</h5>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Component, Vue } from "vue-property-decorator";
+import HelloWorld from "@/components/HelloWorld.vue";
+import json from "@/quotes.json";
 
 @Component({
   components: {
-    HelloWorld,
-  },
+    HelloWorld
+  }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+  quotes: any = json.quotes;
+  quote: any = this.quotes[0];
+}
 </script>
